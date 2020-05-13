909a4d917236eca95d524e7dafb1af572a438cbe
RuchiBhardwaj <ruchi.bhardwaj@nineleaps.com>
2020-05-13 19:01:17 +0530

    adding functinalities

---
+++ AIMS/Employee.py
@@ -0,0 +1,78 @@
+import uuid
+import AIMS.Repository as repo
+from sqlite3 import Error
+import datetime
+
+connection = repo.sql_connection()
+
+
+class employee:
+    def __init__(self, role_id):
+        self.role_id = role_id
+
+    def selection(self):
+        ch = ''
+        while ch != 3:
+            ch = input("choose 1.File complain 2.Show complain 3.Exit: ")
+            if ch == '1':
+                self.complain_file()
+            elif ch == '2':
+                self.show_complain()
+            elif ch == '3':
+                return
+            else:
+                print("invalid choice")
+
+    def complain_file(self):
+        try:
+            print("file your complain here")
+            created_at = datetime.datetime.today()
+            complain_id = str(uuid.uuid4())
+            description = input("Enter all the details about incident please: ")
+            working_zone = input("Enter working zone where incident happened: ")
+            if description and working_zone:
+                accident_list = ["fire breakout", "gas leakage", "infrastructure damages", "other"]
+                complain_type = None
+                print("Choose complain type \n 1.Fire Breakout 2.Gas Leakage 3.Infrastructure Damages 4.Other")
+                ch = (input("Write your complain type: ")).lower()
+                while not ch in accident_list:
+                    print("Entered complain type doesnt exist")
+                    ch = (input("Write your complain type: ")).lower()
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT into complains(complain_id,description,working_zone,role_id,status,created_at,"
+                    "delete_value,verdict,complain_type) Values(\'{}\',\'{}\', "
+                    "\'{}\', \'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')".format(complain_id, description, working_zone,
+                                                                                self.role_id,
+                                                                                "open", created_at, "False", 'False',
+                                                                                complain_type))
+                connection.commit()
+                cursor.close()
+                print("Your complain is filed")
+                return True
+            print("Description or working zone is empty")
+            return False
+        except Error as e:
+            print(e)
+
+    def show_complain(self):
+        try:
+            cursor = connection.cursor()
+            records = cursor.execute(
+                "select complain_id,description,working_zone,status,created_at from complains where delete_value = "
+                "\'False\' and role_id = \'{}\'".format(self.role_id)).fetchall()
+            if records:
+                print("All complains")
+                for row in records:
+                    print("Complain_id= ", row[0])
+                    print("Description= ", row[1])
+                    print("Working_zone= ", row[2])
+                    print("Status= ", row[3])
+                    print("Date of incident= ", row[4])
+                    print("-----------------------")
+                return True
+            print("No complains found")
+            return True
+        except Error as e:
+            print(e)
+            return False
---
+++ AIMS/Login.py
@@ -0,0 +1,83 @@
+from cryptography.fernet import Fernet
+import getpass
+import AIMS.Repository as repo
+from sqlite3 import Error
+
+from AIMS.Admin import admin
+from AIMS.Employee import employee as emp
+from AIMS.SupervisingTeam import supervising_team
+
+connection = repo.sql_connection()
+cursor = connection.cursor()
+
+
+class login:
+    def __init__(self):
+        try:
+            file = open('key.txt', 'rb')
+            key = file.read()
+            self.cipher_suite = Fernet(key)
+            file.close()
+        except IOError or FileNotFoundError as e:
+            print(e)
+
+    def check_admin(self):
+        username = input("Enter your username: ")
+        password = getpass.getpass('Enter your password: ')
+        if username and password:
+            try:
+                retrieve_password = cursor.execute(
+                    "select * from login where username = \'{}\' and role_name = \'admin\'".format(username)).fetchall()
+                connection.commit()
+                cursor.close()
+                if retrieve_password:
+                    if password.encode() == self.cipher_suite.decrypt(retrieve_password[0][1].encode()):
+                        admin().select_choice()
+                        return "Access granted"
+                    return "wrong password"
+                return "Username not found"
+            except IOError or getpass.GetPassWarning or Error as e:
+                return e
+        return "Password or Username is empty"
+
+    def check_emp(self):
+        username = input("Enter your username: ")
+        password = getpass.getpass('Enter your password: ')
+        if username and password:
+            try:
+                retrieve_password = cursor.execute(
+                    "select * from login where username = \'{}\' and role_name = \'employee\'".format(
+                        username)).fetchall()
+                connection.commit()
+                cursor.close()
+                if retrieve_password:
+                    if password.encode() == self.cipher_suite.decrypt(retrieve_password[0][1].encode()):
+                        emp(retrieve_password[0][3]).selection()
+                        return "Access granted"
+                    return "wrong password"
+                return "Username not found"
+            except IOError or getpass.GetPassWarning or Error as e:
+                return e
+        return "Password or Username is empty"
+
+    def check_team(self):
+        team_name = input("Enter your Team name: ")
+        password = getpass.getpass('Enter your team password: ')
+        if team_name and password:
+            try:
+                retrieve_password = cursor.execute(
+                    "select * from supervising_team where team_name = \'{}\'".format(team_name)).fetchall()
+                connection.commit()
+                cursor.close()
+                if retrieve_password:
+                    if password.encode() == self.cipher_suite.decrypt(retrieve_password[0][6].encode()):
+                        supervising_team(retrieve_password[0][0]).selection()
+                        return "Access granted"
+                    return "wrong password"
+                return "Team id not found"
+            except IOError or getpass.GetPassWarning or Error as e:
+                return e
+
+
+# log = login()
+# print(log.check_team())
